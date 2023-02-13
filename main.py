7920ba98c433fbf46937dca56189470e62eb5a03
DvDeng <andersdavid.engstrom@gmail.com>
2023-02-13 16:07:23 +0100

    plays för ordergenerator

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import math
-class order:
+from queue import PriorityQueue
+
+class Order:
     def __init__(self, items):
         self.items = items
 
-class station:
+class Station:
     def __init__(self, max_orders = 1, max_buffers=0):
         self.max_orders = max_orders
         self.max_buffers = max_buffers
         self.orders = []
         self.buffers = [] 
-    
+
+event_queue = PriorityQueue()
+
+#nån loop för att generera ordrar
 
 
-time_to_get = [1+0.01*x for x in range(1000)] 
-number_of_pallets = [10]*1000
+items = [1,2,3]
+order = Order(items)
+event_queue.put((2,order))
+
+
+
+time_to_get = [60+0.6*x for x in range(1000)]
+
+#nån fördelning av tid att hämta varor
+
+
+
+
+
+#slut
+
 
 stations = [] 
 order_queue = []
