c5a1ff57f8cbfe96569eb0c52baf44b8cde70d77
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-02-13 16:21:26 +0100

    first pareto code

--- m.py
+++ m.py
@@ -1,13 +1,12 @@
 import random
 
+
 class Order:
     def __init__(self, items):
         self.items = items
 
-        
 
-
-#nån fördelning av tid att hämta varor
+# nån fördelning av tid att hämta varor
 
 def sample_pareto(probabilities, k):
     cumulative_probability = 0
@@ -25,17 +24,19 @@ def sample_pareto(probabilities, k):
                 break
     return result
 
+
 time_to_get = [1 + 0.01 * x for x in range(1000)]
 
-def nbrItemsInOrder():
-    
 
-# Generate a list of probabilities that follow the Pareto distribution
+def nbrItemsInOrder():
+
+    # Generate a list of probabilities that follow the Pareto distribution
 n = len(time_to_get)
 alpha = 1.16
 probabilities = [0] * n
 for i in range(n):
-    probabilities[i] = (i + 1) ** (-alpha) / sum(j ** (-alpha) for j in range(1, n + 1))
+    probabilities[i] = (i + 1) ** (-alpha) / sum(j ** (-alpha)
+                                                 for j in range(1, n + 1))
 
 # Use the probabilities to sample 20 items from the time_to_get list
 pareto_sample = sample_pareto(probabilities, nbrItemsInOrder)
